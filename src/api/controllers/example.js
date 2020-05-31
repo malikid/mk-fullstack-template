c52bed350e0fd9da08cd47112fd5e597b61914af
malikid <malikid.chang@gmail.com>
2020-05-31 15:31:31 +0200

    Fixed a bug in the example.

--- src/api/controllers/example.js
+++ src/api/controllers/example.js
@@ -1,7 +1,7 @@
 const index = (req, res) => res.send('Hello World from example controller index, which is the default entry point!');
 const example = (req, res) => {
-  console.log("Hello World from example controller example!");
-  Example.aaa(req, res);
+  console.log('Hello World from example controller example!');
+  ExampleService.aaa(req, res);
 };
 
 export {index, example};
